959f2bd94c06f2e9e88a75013eddb393c10a9282
Makushi <maxivicraba@hotmail.com>
2017-06-13 21:57:17 -0300

    Movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform target;
+	private Vector3 posDiff = Vector3.zero;
+
+	void Start () 
+	{
+		posDiff = transform.position - target.position;	
+	}
+		
+	void Update ()
+	{
+		transform.position = target.position + posDiff;
+	}
+
+}
---
+++ Assets/Scripts/LanderMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class LanderMovement : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = transform.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			rb.AddRelativeForce (Vector2.up * Time.deltaTime * 50);
+		}
+
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			rb.MoveRotation(rb.rotation - 50 * Time.fixedDeltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			rb.MoveRotation(rb.rotation + 50 * Time.fixedDeltaTime);
+		}
+	}
+}
